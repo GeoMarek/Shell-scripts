a1fffcafa183442438841a1efacd06e3bc5588d0
Marek Grudkowski <mar.grudkowski@gmail.com>
2020-09-30 21:21:39 +0200

    Add powershell recursive tree v0.1

---
+++ win_file_tree.ps1
@@ -0,0 +1,45 @@
+﻿function Get-DirectoryTree
+{
+    param
+    (
+        [Parameter(mandatory=$true)][string]$Path
+        #[Parameter(mandatory=$true)][int]$Depth
+    )
+
+    # get all items from $Path
+    $items = Get-ChildItem $Path
+    
+    
+    foreach($item in $items)
+    {
+        if ($item.Mode.contains('d'))
+        {
+            "directory"; $item.FullName
+        }
+        else
+        {
+            "File"; $item.FullName
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    # test main item
+    if (Test-Path -Path $Path -PathType Container) 
+    {
+        "directory"
+    }
+    else 
+    {
+            
+        "file"
+    }
+
+}
+
+
+Get-DirectoryTree -Path $args[0] 
